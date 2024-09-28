6e86de413641aa5171f92edff841b507f5cce5e3
Alonso <jalonsojic@gmail.com>
2024-09-28 17:36:15 -0600

    WorkFlow, v0.1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,30 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { TasksComponent } from './pages/tasks/tasks.component';
 
 export const routes: Routes = [
-/*     {
+    {
         path: 'home',
         component: HomeComponent
-    }, */
+    },
+    {
+        path: 'tasks',
+        component: TasksComponent
+    },
+    {
+        path: 'not-found',
+        component: HomeComponent
+    },
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+
+      //RENDERIZA LA PAGE NOTFOUND.... SIEMPRE VA DE ULTIMO PORQUE INTERFIERE!....
+    {
+    path: '**',
+    component: NotFoundComponent
+    },
 ];
