2620d349f8f8ba3ee45b3bba07acea88cfa9a5b7
Alonso <jalonsojic@gmail.com>
2024-10-02 21:19:37 -0600

    WorkFlow, v0.2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { TasksComponent } from './pages/tasks/tasks.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {
@@ -11,6 +12,10 @@ export const routes: Routes = [
     {
         path: 'tasks',
         component: TasksComponent
+    },
+    {
+        path: 'dashboard',
+        component: DashboardComponent
     },
     {
         path: 'not-found',
