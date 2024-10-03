45728049a17ac96096ccf634d54394e2eac06cc2
Alonso <jalonsojic@gmail.com>
2024-10-03 03:57:29 -0600

    Workflow v.0.3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { TasksComponent } from './pages/tasks/tasks.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
         path: 'home',
         component: HomeComponent
-    },
-    {
-        path: 'tasks',
-        component: TasksComponent
     },
     {
         path: 'dashboard',
@@ -20,6 +17,14 @@ export const routes: Routes = [
     {
         path: 'not-found',
         component: HomeComponent
+    },
+    {
+        path: 'login',
+        component: LoginComponent
+    },
+    {
+        path: 'register',
+        component: RegisterComponent
     },
     {
         path: '',
