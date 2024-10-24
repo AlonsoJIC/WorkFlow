0d32d9a4b72af9b80f7effc8d5a5719d413a8909
Alonso <jalonsojic@gmail.com>
2024-10-24 16:31:35 -0600

    v0.5- error inject

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,46 +1,37 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './modules/home/home/home.component';
 import { NotFoundComponent } from './modules/home/not-found/not-found.component';
-import { DashboardComponent } from './modules/dashboard/dashboard/dashboard.component';
-import { LoginComponent } from './modules/auth/login/login.component';
-import { RegisterComponent } from './modules/auth/register/register.component';
+import { AuthGuard } from './guards/auth.guard';
+import { RedirectGuard } from './guards/redirect.guard';
 
 export const routes: Routes = [
     {
-      path: 'auth',
-      // canActivate: [AuthGuard],
-      loadChildren: async () =>
-        (await import('./modules/auth/auth-routing.module')).routes,
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+    },
+    {
+        path: '',
+        canActivate: [RedirectGuard],
+        loadChildren: () => import('./modules/auth/auth-routing.module').then((m) => m.AuthRoutingModule),
+    },
+    {
+        path: 'dashboard',
+        canActivate: [AuthGuard],
+        loadChildren: () => import('./modules/home/home-routing.module').then((m) => m.HomeRoutingModule),
     },
     {
         path: 'home',
         component: HomeComponent
     },
-    {
-        path: 'dashboard',
-        component: DashboardComponent
-    },
+
     {
         path: 'not-found',
-        component: HomeComponent
+        component: NotFoundComponent
     },
-    // {
-    //     path: 'login',
-    //     component: LoginComponent
-    // },
-    // {
-    //     path: 'register',
-    //     component: RegisterComponent
-    // },
+    //RENDERIZA LA PAGE NOTFOUND.... SIEMPRE VA DE ULTIMO PORQUE INTERFIERE!....
     {
-        path: '',
-        redirectTo: 'auth',
-        pathMatch: 'full'
-    },
-
-      //RENDERIZA LA PAGE NOTFOUND.... SIEMPRE VA DE ULTIMO PORQUE INTERFIERE!....
-    {
-    path: '**',
-    component: NotFoundComponent
+        path: '**',
+        component: NotFoundComponent
     },
 ];
--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,12 +1,88 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import { CustomValidators } from '../../../models/validators';
+import { RequestStatus } from '../../../models/request-status.model';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  formBuilder = inject(FormBuilder);
 
+  formUser = this.formBuilder.nonNullable.group({
+    email: ['', [Validators.email, Validators.required]],
+  });
+
+  form = this.formBuilder.nonNullable.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6), Validators.maxLength(18)]],
+    confirmPassword: ['', [Validators.required]],
+  }, {
+    validators: [ CustomValidators.MatchValidator('password', 'confirmPassword') ]
+  });
+  status: RequestStatus = 'init';
+  statusUser: RequestStatus = 'init';
+  faEye = faEye; faEyeSlash = faEyeSlash; showPassword = false; showRegister = false;
+
+  constructor(
+/*     private formBuilder: FormBuilder,
+ */    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  register() {
+    if (this.form.valid) {
+      this.status = 'loading';
+      const { name, email, password } = this.form.getRawValue();
+      this.authService.registerAndLogin(name, email, password)
+      .subscribe({
+        next: () => {
+          this.status = 'success';
+          this.router.navigate(['/profile']);
+        },
+        error: (error) => {
+          this.status = 'failed';
+          console.log(error);
+        }
+      })
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  validateUser() {
+    if (this.formUser.valid) {
+      this.statusUser = 'loading';
+      const { email } = this.formUser.getRawValue();
+      this.authService.isAvailable(email)
+      .subscribe({
+        next: (rta) => {
+          this.statusUser = 'success';
+          if (rta.isAvailable) {
+            this.showRegister = true;
+            this.form.controls.email.setValue(email);
+          } else {
+            this.router.navigate(['/login'], {
+              queryParams: { email }
+            });
+          }
+        },
+        error: (error) => {
+          this.statusUser = 'failed';
+          console.log(error);
+        }
+      })
+    } else {
+      this.formUser.markAllAsTouched();
+    }
+  }
 }
