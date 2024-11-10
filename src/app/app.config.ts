f1b6694d0d9e428b83a24b00e3626df94e048c14
Gaelor06 <Gaelor06@gmail.com>
2024-11-09 23:01:03 -0600

    v0.13 - Sidebar actualizada

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,6 +6,6 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),     provideHttpClient(), provideAnimationsAsync()  // Proporciona el cliente HTTP de manera global
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),     provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()  // Proporciona el cliente HTTP de manera global
   ]
 };
